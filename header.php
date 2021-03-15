50b104a4eef9fc4ef46f6318004d8c27983b8e12
Derelict <oleksandr.dzyndzyura@gmail.com>
2021-03-15 17:29:27 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php get_template_part( 'templates/partials/document-open' ); ?>
+
+<!-- Site header markup goes here -->
+<div class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
+    <div class="flex-1 flex justify-between items-center">
+        <a href="#">
+            <svg width="32" height="36" viewBox="0 0 32 36" xmlns="http://www.w3.org/2000/svg"><path d="M15.922 35.798c-.946 0-1.852-.228-2.549-.638l-10.825-6.379c-1.428-.843-2.549-2.82-2.549-4.501v-12.762c0-1.681 1.12-3.663 2.549-4.501l10.825-6.379c.696-.41 1.602-.638 2.549-.638.946 0 1.852.228 2.549.638l10.825 6.379c1.428.843 2.549 2.82 2.549 4.501v12.762c0 1.681-1.12 3.663-2.549 4.501l-10.825 6.379c-.696.41-1.602.638-2.549.638zm0-33.474c-.545 0-1.058.118-1.406.323l-10.825 6.383c-.737.433-1.406 1.617-1.406 2.488v12.762c0 .866.67 2.05 1.406 2.488l10.825 6.379c.348.205.862.323 1.406.323.545 0 1.058-.118 1.406-.323l10.825-6.383c.737-.433 1.406-1.617 1.406-2.488v-12.757c0-.866-.67-2.05-1.406-2.488l-10.825-6.379c-.348-.21-.862-.328-1.406-.328zM26.024 13.104l-7.205 13.258-3.053-5.777-3.071 5.777-7.187-13.258h4.343l2.803 5.189 3.107-5.832 3.089 5.832 2.821-5.189h4.352z"></path></svg>
+        </a>
+
+    </div>
+    <label for="menu-toggle" class="pointer-cursor lg:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+    <input class="hidden" type="checkbox" id="menu-toggle" />
+    <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+        <nav>
+            <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Features</a></li>
+                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Pricing</a></li>
+                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Documentation</a></li>
+                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="#">Support</a></li>
+                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="#">Support</a></li>
+                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="#">Support</a></li>
+                <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="#">Support Support Support</a></li>
+            </ul>
+        </nav>
+        <div class="relative text-gray-600">
+            <input type="search" name="serch" placeholder="Search" class="bg-white shadow-xl h-10 px-5 pr-10 rounded-full text-sm focus:outline-none">
+            <button type="submit" class="absolute shadow-xl right-0 top-0 mt-3 mr-4">
+                <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
+                     <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
+              </svg>
+            </button>
+        </div>
+    </div>
+</div>
---
+++ templates/partials/example.php
@@ -0,0 +1,180 @@
+<section class="hero text-gray-600 body-font">
+    <div class="flex bg-white" style="height:600px;">
+        <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
+            <div>
+                <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                <p class="mt-2 text-sm text-gray-500 md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates. Cumque debitis dignissimos id quam vel!</p>
+                <div class="flex justify-center lg:justify-start mt-6">
+                    <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">Get Started</a>
+                    <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="#">Learn More</a>
+                </div>
+            </div>
+        </div>
+        <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
+            <div class="h-full object-cover" style="background-image: url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80)">
+                <div class="h-full bg-black opacity-25"></div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap w-full mb-20">
+            <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+                <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+            </div>
+            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+        </div>
+        <div class="flex flex-wrap -m-4">
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full  object-center mb-6" src="<?php bloginfo('template_directory'); ?> /assets/img/screen.svg" alt="content">
+                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">Спеціалізація - "Обслуговування комп'ютерних систем і мереж"</h3>
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Комп'ютерна інженерія</h2>
+                    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full object-center mb-6" src="<?php bloginfo('template_directory'); ?> /assets/img/car-compact.svg" alt="content">
+                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">"Спеціалізація - "Обслуговування та ремонт автомобілів і двигунів"</h3>
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">"АВТОМОБІЛЬНИЙ ТРАНСПОРТ"</h2>
+                    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full  object-center mb-6" src="<?php bloginfo('template_directory'); ?> /assets/img/presentation.svg" alt="content">
+                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">Спеціалізація -"Комерційна діяльність"</h3>
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">"МАРКЕТИНГ".</h2>
+                    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full  object-center mb-6" src="<?php bloginfo('template_directory'); ?> /assets/img/plug.svg" alt="content">
+                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">Спеціалізація -"Монтаж і експлуатація електроустаткування підприємств і цивільних споруд"</h3>
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">"ЕЛЕКТРОЕНЕРГЕТИКА, ЕЛЕКТРОТЕХНІКА та ЕЛЕКТРОМЕХАНІКА"</h2>
+                    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="min-w-screen  bg-gray-100 flex items-center justify-center bg-gray-100">
+    <div class="max-w-7xl w-full mx-auto py-6 sm:px-6 lg:px-64">
+        <div class="flex flex-col lg:flex-row w-full lg:space-x-2 space-y-2 lg:space-y-0 mb-2 lg:mb-4">
+
+            <div class="w-full lg:w-1/5">
+                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-purple-400">
+                    <div class="flex items-center">
+                        <div class="icon w-14 p-3.5 bg-purple-400 text-white rounded-full mr-3">
+                            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
+                            </svg>
+                        </div>
+                        <div class="flex flex-col justify-center">
+                            <div class="text-lg">230k</div>
+                            <div class="text-sm text-gray-400">Sales</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full lg:w-1/4">
+                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-blue-400">
+                    <div class="flex items-center">
+                        <div class="icon w-14 p-3.5 bg-blue-400 text-white rounded-full mr-3">
+                            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>
+                        </div>
+                        <div class="flex flex-col justify-center">
+                            <div class="text-lg">3456</div>
+                            <div class="text-sm text-gray-400">Customers</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full lg:w-1/4">
+                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-yellow-400">
+                    <div class="flex items-center">
+                        <div class="icon w-14 p-3.5 bg-yellow-400 text-white rounded-full mr-3">
+                            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                            </svg>
+                        </div>
+                        <div class="flex flex-col justify-center">
+                            <div class="text-lg">3456</div>
+                            <div class="text-sm text-gray-400">Products</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full lg:w-1/4">
+                <div class="widget w-full p-4 rounded-lg bg-white border-l-4 border-red-400">
+                    <div class="flex items-center">
+                        <div class="icon w-14 p-3.5 bg-red-400 text-white rounded-full mr-3">
+                            <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                        </div>
+                        <div class="flex flex-col justify-center">
+                            <div class="text-lg">12658</div>
+                            <div class="text-sm text-gray-400">Orders</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">Наш Колектив</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+        </div>
+        <div class="flex flex-wrap -m-4">
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Лашків Богдан Ярославович</h2>
+
+                </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content">
+
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Кулик Ірина Ярославівна</h2>
+
+                </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content">
+
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Чорний Микола Васильович</h2>
+
+                </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+                <div class="bg-gray-100 p-6 rounded-lg">
+                    <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content">
+
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Білоус Тетяна Анатолівна</h2>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
